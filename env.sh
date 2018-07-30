a48e6f254d0114366f459a9704150ac75c44923c
akhan4u <aman.khan@globallogic.com>
2018-07-30 12:03:49 +0530

    MY FIRST COMMIT

---
+++ env.sh
@@ -0,0 +1,3 @@
+JVM_OPTS="$JVM_OPTS -javaagent:/data/data/jmx_prometheus_javaagent-0.3.1.jar=7070:/data/data/cassandra_new_metrics.yml"
+#JVM_OPTS="$JVM_OPTS -javaagent:/data/data/jmx_prometheus_javaagent-0.3.1.jar=7070:/data/data/prometheus_cassandra.yml"
+
